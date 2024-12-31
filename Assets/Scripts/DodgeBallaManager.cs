992a11b1df177145f05c4a3d09914c945f5a6351
Dylan Mather <116815437+DylanMather2004@users.noreply.github.com>
2024-12-31 15:19:55 +0000

    More fixing packages
    Because unity loves to delete them -_-

---
+++ Assets/Scripts/DodgeBallaManager.cs
@@ -0,0 +1,22 @@
+using Unity.Netcode; 
+using UnityEngine;
+
+
+namespace DodgeBalla
+{
+    public class DodgeBallaManager : MonoBehaviour
+    {
+        private NetworkManager networkManager;
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+

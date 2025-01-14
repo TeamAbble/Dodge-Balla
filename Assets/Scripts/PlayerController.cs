b11afc691f95c24421f41080bdafd39555d3a083
Dylan Mather <116815437+DylanMather2004@users.noreply.github.com>
2025-01-14 22:20:35 +0000

    Cinemachine Camera implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,35 @@
+using Unity.Cinemachine;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool isGrounded;
-    [Header("Movement Configs")]
-    [SerializeField] private float playerSpeed = 2.0f,jumpHeight = 1.0f,gravity=-9.81f;
+    [Header("Movement Settings")]
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravity=-9.81f;
     private InputManager inputManager;
+
+    [Header("Camera Settings")]
+    [SerializeField] private CinemachineCamera cam;
    
     void Start()
     {
         controller = GetComponent<CharacterController>();
         inputManager = GetComponent<InputManager>();
     }
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        if (!IsOwner)
+        {
+            cam.enabled = false;   
+        }
+    }
+
 
     // Update is called once per frame
     void Update()

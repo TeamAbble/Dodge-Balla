e2e746117d69fc9206c2d7ee7dda78783f38422b
Dylan Mather <116815437+DylanMather2004@users.noreply.github.com>
2025-01-15 01:36:37 +0000

    FPS Camera system implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Cinemachine;
 using Unity.Netcode;
 using UnityEngine;
@@ -15,7 +16,10 @@ public class PlayerController : NetworkBehaviour
 
     [Header("Camera Settings")]
     [SerializeField] private CinemachineCamera cam;
-   
+    [SerializeField] private GameObject head;
+    [SerializeField] private float clampAngle = 80f;
+    [SerializeField] private float lookSpeed = 300f;
+    private Vector3 startRotation; 
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -34,6 +38,8 @@ public class PlayerController : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!IsOwner) return;
+        LookAround();
         isGrounded = controller.isGrounded;
         if (isGrounded && playerVelocity.y < 0)
         {
@@ -41,20 +47,24 @@ public class PlayerController : NetworkBehaviour
         }
         Vector2 movement = inputManager.GetPlayerMovement();
         Vector3 move = new Vector3(movement.x, 0f, movement.y);
+        move = head.transform.forward*move.z+ head.transform.right*move.x;
+        move.y = 0;
         controller.Move(move*Time.deltaTime*playerSpeed);
 
-        if (move != Vector3.zero)
-        {
-            //rotates the player in the movement direction
-            gameObject.transform.forward = move;
-        }
-        if (inputManager.PlayerJumped() && isGrounded)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
-        }
         playerVelocity.y += gravity*Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
 
+
+    }
+
+    void LookAround()
+    {
+        if (startRotation == null) startRotation = head.transform.localRotation.eulerAngles;
+        Vector2 mouseInput=inputManager.GetMouseDelta();
+        startRotation.x += mouseInput.x * Time.deltaTime*lookSpeed;
+        startRotation.y += mouseInput.y * Time.deltaTime*lookSpeed;
+        startRotation.y = Mathf.Clamp(startRotation.y, -clampAngle, clampAngle);
+        head.transform.rotation = Quaternion.Euler(-startRotation.y,startRotation.x,0f );
 
     }
 }
